5110f1bf02448b76924014549cac652c49f81796
Danilo Molina <d.molina@spryflash.com>
2020-03-12 11:43:52 +0000

    Update background image and implement SafeArea for iOS

---
+++ MyWallet/Effects/SafeAreaPaddingEffect.cs
@@ -0,0 +1,11 @@
+using System;
+using Xamarin.Forms;
+
+namespace MyWallet.Effects
+{
+    public class SafeAreaPaddingEffect : RoutingEffect
+    {
+        public SafeAreaPaddingEffect() : base("MyWallet.SafeAreaPaddingEffect")
+        { }
+    }
+}
